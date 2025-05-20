c3282ed3c910e7585e79e1d0ede27b4188c3aa0f
David Pineda <deibittop@gmail.com>
2025-05-20 01:53:17 +0000

    first commit test

---
+++ App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import profile from './components/profile';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Profile/>
+
+     </div>
+  );
+}
+
+export default App;
---
+++ components/custom_buttom.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+
+
+export default Custom_Buttom = () => {
+        const profile_dict = {
+
+        "kumatanni": this_pro_url ,
+        "ebara" : this_pro_url ,
+        "Yui" : this_pro_url ,
+        "wada" : this_pro_url,
+        "kan" : this_pro_url,
+        "choumanka" : this_pro_url ,
+        "david" : this_pro_url 
+
+    };
+
+
+    return (
+        <div>
+            <button onClick={}>
+                プロフィールを参照
+            </button>
+        </div>
+    );
+
+}
